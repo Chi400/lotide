5be16e0cd51fa65d9686f74ccaa246c8d53d8ca1
Marie D <desjardinseve@gmail.com>
2023-01-10 01:24:48 +0000

    added arrays tail.js

---
+++ tail.js
@@ -0,0 +1,25 @@
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+const tail = function(array) {
+  let tailArr = array.slice(1);
+  return tailArr;
+};
+
+assertEqual(tail([5,6,7]), [6,7]);
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
