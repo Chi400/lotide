4e3cef3764e83ee071614fc84036f88b314355ae
Marie D <desjardinseve@gmail.com>
2023-01-11 19:13:10 +0000

    added assertObjectEquals.js

---
+++ assertObjectsEqual.js
@@ -0,0 +1,9 @@
+
+
+
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (!eqObjects(actual, expected)) return (console.log(`❌❌❌ Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`));
+  return (console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`));
+};
